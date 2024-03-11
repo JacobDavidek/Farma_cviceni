a07e10cf0b88c40aff93802f155aa0b5361b2cbc
davidek <unluckycz@gmail.com>
2024-03-11 08:29:16 +0100

    Přidání konstruktoru do třídy beFlower a vytvoření třídy Strawberry

--- src/Flower.java
+++ src/Flower.java
@@ -5,6 +5,22 @@ public abstract class Flower {
     protected double neededArea;
     protected double chanceOfGrowth;
 
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public void irrigate(){
+        chanceOfGrowth += 0.1;
+    }
+
+
+
+
+
+
     @Override
     public String toString() {
         return "Flower{" +
---
+++ src/Strawberry.java
@@ -0,0 +1,8 @@
+public class Strawberry extends Flower{
+
+   public Strawberry(){
+      super("Strawberry",50,1,0.8);
+   }
+
+
+}
