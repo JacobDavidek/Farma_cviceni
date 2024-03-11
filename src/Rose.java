a68fc82e97c396d0a4204d88290b60f8f975147e
davidek <unluckycz@gmail.com>
2024-03-11 08:36:55 +0100

    Vytvoření tříd Tulip a Rose

---
+++ src/Rose.java
@@ -0,0 +1,6 @@
+public class Rose extends Flower {
+
+    public Rose(){
+        super("Rose",30,2,0.5);
+    }
+}
---
+++ src/Tulip.java
@@ -0,0 +1,7 @@
+public class Tulip extends Flower{
+
+    public Tulip(){
+        super("Tulip",10,1,0.7);
+    }
+
+}
